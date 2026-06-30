fa1d85165c4f7240a1978bd48ec92c3b6454e33b
debayan66 <debayannath619@gmail.com>
2026-06-30 15:51:56 -0400

    Build CarModel and CarMake Django Models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     # path(route='login', view=views.login_user, name='login'),
     path(route='login', view=views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),
-    path('register', views.registration, name='registration')
+    path('register', views.registration, name='registration'),
+    path(route='get_cars', view=views.get_cars, name ='getcars')
+
 
 
     # path for dealer reviews view
